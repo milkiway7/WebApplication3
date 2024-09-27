26fb72c16532ea774730ee34b1ed741d2f60520e
Alex Mleczko <alex.mleczko@arc-consulting.pl>
2024-09-27 12:02:29 +0200

    Add project files.

---
+++ WebApplication3/src/Components/Home.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Home = () => {
+    return (
+        <div id="root">
+            <h1>Co jest kurwa</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ WebApplication3/webpack.config.js
@@ -0,0 +1,26 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // Główne wejście Reacta
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/js'), // Wyjściowy katalog
+        filename: 'bundle.js', // Nazwa wyjściowego pliku
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // Przetwarza pliki JS/JSX
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'], // Użyj Babel do JSX
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], // Obsługuje pliki JS i JSX
+    },
+};
