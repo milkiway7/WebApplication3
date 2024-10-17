7fc15ea3d19957f71894305905fe68a107d773ec
Alex Mleczko <alex.mleczko@arc-consulting.pl>
2024-10-17 13:08:40 +0200

    Draft model -> ModelAddProject

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -1,7 +1,14 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PmcConnectionString"));
+});
 
 var app = builder.Build();
 
