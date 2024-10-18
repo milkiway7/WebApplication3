e0e5cc9d108cb9ccc1853febd43cc3588cd87296
Alex Mleczko <alex.mleczko@arc-consulting.pl>
2024-10-18 10:59:20 +0200

    Swagger

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -1,4 +1,5 @@
 using DataAccess;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("PmcConnectionString"));
 });
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -19,6 +21,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
