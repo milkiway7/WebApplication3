dc9ddcc2231d139e516270a70236f3ca6ce214a5
Alex Mleczko <alex.mleczko@arc-consulting.pl>
2024-10-23 12:27:37 +0200

    Authorization -> Register, log in, log out

--- DataAccess/Models/Authentication/SignInPageModel.cs
+++ DataAccess/Models/Authentication/SignInPageModel.cs
@@ -8,21 +8,19 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Models.Authentication
 {
-    public class RegisterPageModel
+    public class SignInPageModel
     {
-        [Required(ErrorMessage = "Email address is required")]
+        [Required]
         [DataType(DataType.EmailAddress)]
         [DisplayName("Email address")]
         public string EmailAddress { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Confirm password is required")]
-        [DataType(DataType.Password)]
-        [Compare(nameof(Password), ErrorMessage ="Password did not match")]
-        [DisplayName("Confirm password")]
-        public string ConfirmPassword { get; set; }
+        [DisplayName("Remember me")]
+        public bool RememberMe { get; set; }
+
     }
 }
